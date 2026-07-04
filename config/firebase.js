4a22b188eb87fdbfbdb7250585a57df6a0dc8801
Doni-jon <codingasik24@gmail.com>
2026-07-04 23:21:50 +0700

    Add files via upload

--- config/firebase.js
+++ config/firebase.js
@@ -4,6 +4,11 @@ const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
   ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
   : require("../serviceAccountKey.json");
 
+// Debug credential
+console.log("Firebase Project:", serviceAccount.project_id);
+console.log("Firebase Email:", serviceAccount.client_email);
+console.log("Has Private Key:", !!serviceAccount.private_key);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
