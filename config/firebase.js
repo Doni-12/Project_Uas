c2337cdce400bc3806c6d2fe9f223b197fcb82d8
Doni-jon <codingasik24@gmail.com>
2026-07-04 23:01:43 +0700

    Add files via upload

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,11 @@
 const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(
-  process.env.firebase_service_account
+  process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const db = admin.firestore();
-
-module.exports = db;
+module.exports = admin.firestore();
